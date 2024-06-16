a25b9dc790d1d1f55127618e3b1dc7b4d226ca30
Kacper Klassa <kacperklassa@gmail.com>
2024-06-16 06:52:44 +0200

    Use Depends mechanism

--- api/app/main.py
+++ api/app/main.py
@@ -30,26 +30,45 @@ text_embedding_model = TextEmbeddingModel.from_pretrained("text-embedding-004")
 
 storage_client = storage.Client(credentials=credentials, project=credentials.project_id)
 
-bucket_name = 'hackwarsaw-fintech-ai-assistant'
-csv_filename = 'biznes-gov-pl-embeddings.csv'
+bucket_name = "hackwarsaw-fintech-ai-assistant"
+csv_filename = "biznes-gov-pl-embeddings.csv"
 
 bucket = storage_client.bucket(bucket_name)
 blob = bucket.blob(csv_filename)
 blob.download_to_filename(csv_filename)
 
-df = pd.read_csv('biznes-gov-pl-embeddings.csv', sep=';')
-df['content_embedding'] = df['content_embedding'].apply(lambda row: literal_eval(row))
-df = df[df['content_embedding'].apply(lambda x: len(x) == 768)]
+df = pd.read_csv("biznes-gov-pl-embeddings.csv", sep=";")
+df["content_embedding"] = df["content_embedding"].apply(lambda row: literal_eval(row))
+df = df[df["content_embedding"].apply(lambda x: len(x) == 768)]
 
 app = FastAPI()
+
+
+def get_model():
+    return model
+
+
+def get_text_embedding_model():
+    return text_embedding_model
+
+
+def get_data():
+    return df
 
 
 @app.get("/")
 async def docs_redirect():
     return RedirectResponse(url="/docs")
 
+
 @app.get("/generate", response_model=str)
-async def generate(query: str, stream: bool = False):
+async def generate(
+    query: str,
+    stream: bool = False,
+    model: GenerativeModel = Depends(get_model),
+    text_embedding_model: TextEmbeddingModel = Depends(get_text_embedding_model),
+    data: pd.DataFrame = Depends(get_data),
+):
     return generate_model_response(
-        model, text_embedding_model, query, df, stream=stream
+        model, text_embedding_model, query, data, stream=stream
     )
